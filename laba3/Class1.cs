4aff5d6d75b6769bff43a93ef115806d7c98581d
MrArtga <artga03@gmail.com>
2021-12-20 14:24:56 +0300

    Work with List

---
+++ laba3/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba3
+{
+    /// <summary>
+    /// Класс, реализующий однонаправленный одномерный список
+    /// </summary>
+    /// <typeparam name="Type">Тип данных списка</typeparam>
+    public class List<Type>
+    {
+        /// <summary>
+        /// Класс, описывающий элемент списка
+        /// </summary>
+        private class ListItem
+        {
+            public Type Data;
+            public ListItem NextListItem;
+        }
+        private ListItem FirstListItem;
+        private int _length;
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+        }
+        /// <summary>
+        /// Метод добавления элемента в список
+        /// </summary>
+        /// <param name="data">Тип данных</param>
+        public void Add(Type data)
+        {
+            // Создаём новый элемент списка с данными, которые мы передали в качестве аргумента
+            var ListItem = new ListItem { Data = data };
+
+            // Если первого эелмента нет, то создаём его без каких-либо ссылок к другим элементам и возвращаем 
+            if (FirstListItem == null)
+            {
+                FirstListItem = ListItem;
+                _length = 1;
+                return;
+            }
+
+            // В другом слачае создаём новую переменную, хранящую копию 1-го элемента
+            // В результате строк ниже он будет содержать элемент перед тем, который мы создаем.
+            var PrevItem = FirstListItem;
+
+            while (PrevItem.NextListItem != null)
+            {
+                PrevItem = PrevItem.NextListItem;
+            }
+
+            PrevItem.NextListItem = ListItem;
+            _length += 1;
+        }
+        /// <summary>
+        /// Метод удаления элемента из списка
+        /// </summary>
+        /// <param name="index">Индекс удаляемого элемента</param>
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            // Эта переменная предназначена для итерации ниже
+            var itemBeforeTheOneToDelete = FirstListItem;
+
+            // Ищем элдемент списка, перед которым мы хотим удалить элемент
+            for (var i = 0; i < index - 1; i++)
+            {
+                itemBeforeTheOneToDelete = itemBeforeTheOneToDelete.NextListItem;
+            }
+            // Пропускаем ссылку на элемент, который мы хотим удалить
+            itemBeforeTheOneToDelete.NextListItem = itemBeforeTheOneToDelete.NextListItem.NextListItem;
+
+            _length--;
+        }
+        /// <summary>
+        /// Метод проверки индекса
+        /// </summary>
+        /// <param name="index">Проверяемый индекс</param>
+        private void ValidateIndex(int index)
+        {
+            if (FirstListItem == null) throw new Exception("Список пуст");
+            if (index >= _length || index < 0) throw new Exception($"Нету элемента под индексом: {index}");
+        }
+        private Type GetItem(int index)
+        {
+            ValidateIndex(index);
+
+            var item = FirstListItem;
+
+            for (var i = 0; i < index; i++)
+            {
+                item = item.NextListItem;
+            }
+
+            return item.Data;
+        }
+        /// <summary>
+        /// Создаём индексатор
+        /// </summary>
+        /// <param name="index">индекс</param>
+        public Type this[int index]
+        {
+            get
+            {
+                return GetItem(index);
+            }
+        }
+    }
+}
---
+++ laba3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace laba3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intList = new List<int>();
+            var stringList = new List<string>();
+            var random = new Random();
+
+            // Рандомно добавляем числа в диапозоне от -100 до 100
+            for (var i = 0; i < 10; i++) intList.Add(random.Next(-100, 100));
+
+            stringList.Add("Good");
+            stringList.Add("Job");
+            stringList.Add("Юрий");
+            stringList.Add("Викторович");
+            stringList.Add("BruhMoment");
+
+            // Выводим список чисел
+            Console.WriteLine("Список чисел:");
+
+            for (var i = 0; i < intList.Length; i++) Console.Write($"{intList[i]} ");
+
+            Console.WriteLine("");
+            Console.WriteLine("Список строк:");
+
+            // Выводим список строк
+            for (var i = 0; i < stringList.Length; i++) Console.Write($"{stringList[i]} ");
+
+            Console.WriteLine("");
+
+            // Рандомно удаляем элементы
+            try
+            {
+                intList.RemoveAt(random.Next(-1, 12));
+                stringList.RemoveAt(random.Next(-1, 6));
+
+                // Printing again
+                Console.WriteLine("Список чисел:");
+
+                for (var i = 0; i < intList.Length; i++) Console.Write($"{intList[i]} ");
+
+                Console.WriteLine("");
+                Console.WriteLine("Список строк:");
+
+                for (var i = 0; i < stringList.Length; i++) Console.Write($"{stringList[i]} ");
+
+                Console.WriteLine("");
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
+            Console.ReadKey();
+        }
+    }
+}
